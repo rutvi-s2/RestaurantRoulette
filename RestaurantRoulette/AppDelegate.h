fbe5866e5926f3215c908e8ac529e2901ff404c2
Rutvi Shah <rutvims@fb.com>
2021-07-12 12:20:42 -0400

    Initial Commit

---
+++ RestaurantRoulette/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  RestaurantRoulette
+//
+//  Created by rutvims on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RestaurantRoulette/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  RestaurantRoulette
+//
+//  Created by rutvims on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
